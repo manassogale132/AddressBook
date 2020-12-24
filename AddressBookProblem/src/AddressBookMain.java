e6fe0ee202302f74fefd9ced7bd75e6a1d32866f
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-12-24 10:05:48 +0530

    Added : Sort by city.

--- AddressBookProblem/src/AddressBookMain.java
+++ AddressBookProblem/src/AddressBookMain.java
@@ -13,7 +13,6 @@ public class AddressBookMain {
 		System.out.println("1. Add User(s) Information .");
 		System.out.println("2. View User(s) Information .");
 		System.out.println("3. Search Information by City .");
-		System.out.println("4. Search Information by State .");
 		System.out.println(" ");
 		System.out.println("------------------------------------------------");
 	
@@ -43,15 +42,8 @@ public class AddressBookMain {
 			System.out.println("---");
 			break;
 			
-		case 4:
-			System.out.println("---");
-			operations.searchInformationByState();
-			System.out.println("---");
-			break;
-
-			
 	
-		}while(option != 4);
+		}while(option != 3);
 
 	}
 
